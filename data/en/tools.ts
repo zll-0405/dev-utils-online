e1584a2ddf027590d62dc792e444a1b3d8c0c0da
Developer.lulu <zhenglulu_0405@163.com>
2024-09-13 21:52:37 +0800

    add i18n tools.ts file

---
+++ data/en/tools.ts
@@ -0,0 +1,14 @@
+export const tools = [
+    {
+      title: "Base64 Encode/Decode",
+      description:
+        "Easily encode and decode Base64 data with our online utility, so you can transmit your data safely or decode Base64-encoded strings.",
+      link: "/utils/base64",
+    },
+    {
+      title: "Timestamp to Date Converter",
+      description:
+        "Paste Unix timestamps and get a human readable dates. Perfect for developers working with time-based data.",
+      link: "/utils/timestamp",
+    },
+];
---
+++ data/zh/tools.ts
@@ -0,0 +1,14 @@
+export const tools = [
+  {
+    title: "Base64 编码/解码",
+    description:
+      "使用我们的在线工具轻松进行 Base64 数据的编码和解码，以安全地传输数据或解码 Base64 编码的字符串。",
+    link: "/utils/base64",
+  },
+  {
+    title: "时间戳转日期转换器",
+    description:
+      "粘贴 Unix 时间戳，获取可读的日期。非常适合处理基于时间的数据的开发者。",
+    link: "/utils/timestamp",
+  },
+];
